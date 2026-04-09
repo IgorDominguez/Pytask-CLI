4de6a1d61f65a57c0718e43697354b27ca72f1d6
IgorDominguez <igordominguez0079080@gmail.com>
2026-04-09 20:51:40 -0300

    Adicionando os comandos de project
    São eles os: 'project create', 'project list', 'project view' e 'project view --tasks'

--- pytask/main.py
+++ pytask/main.py
@@ -3,7 +3,7 @@ from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
 from rich.prompt import Prompt
-from db.functions import add_task_db, list_tasks, list_unique_task, delete_task, add_project
+from db.functions import add_task_db, list_tasks, list_unique_task, delete_task, add_project_db, list_projects, list_project_unique, list_tasks_project
 import pyfiglet
 
 app = typer.Typer()
@@ -11,6 +11,9 @@ console = Console()
 
 task_app = typer.Typer()
 app.add_typer(task_app, name="task")
+
+project_app = typer.Typer()
+app.add_typer(project_app, name="project")
 
 # pytask
 @app.callback(invoke_without_command=True)
@@ -30,8 +33,9 @@ def main(ctx: typer.Context):
         table.add_row("task view <id da tarefa>", "Mostra uma tarefa específica")
         table.add_row("task delete <id da tarefa>", "Deleta uma tarefa")
         table.add_row("task done <id da tarefa>", "Marca uma tarefa como concluída")
-        table.add_row("project add <nome do projeto>", "Adiciona um novo projeto")
+        table.add_row("project create <nome do projeto>", "Adiciona um novo projeto")
         table.add_row("project list", "Lista todos os projetos")
+        table.add_row("project view <id do projeto>", "Lista as tarefas de um projeto")
 
         console.print(table)
 
@@ -52,7 +56,7 @@ def add_task():
         console.print(Panel(f"{result}", style="bold red"))
         return
 
-    console.print(Panel(f"✓ Tarefa [bold]{title_task}[/bold] adicionada com sucesso!", style="bold blue"))
+    console.print(Panel(f"✓ Tarefa [bold]{title_task}[/bold] adicionada com sucesso!", style="bold green"))
 
 @task_app.command("list")
 def add_task():
@@ -110,6 +114,108 @@ def add_task(id: int):
         
     except:
         console.print(Panel("Esta tarefa ainda não foi criada :(", style="bold red"))
+
+# ... project
+@project_app.command("create")
+def create_project():
+    console.print(Panel("Criar um projeto", style="bold blue"))
+
+    title_project = Prompt.ask("Título do projeto")
+    content_project = Prompt.ask("Descrição (opcional)", default="", show_default=False)
+
+    result = add_project_db(title_project, content_project)
+
+    try:
+        console.print(Panel(f"✓ Projeto [bold]{title_project}[/bold] criado com sucesso!", style="bold green"))
+    except:
+        console.print(Panel(f"Ocorreu um [bold]erro[/bold] durante o processo! Tente novamente", style="bold red"))
+
+@project_app.command("list")
+def create_project():
+    console.print(Panel("Listar todas os projetos", style="bold blue"))
+
+    projects = list_projects()
+
+    try:
+        table = Table(border_style="blue")
+
+        table.add_column("Id", style="white")
+        table.add_column("Nome", style="white")
+        table.add_column("Descrição", style="dim")
+
+        for prj in projects:
+            table.add_row(
+                str(prj["project_id"]),
+                prj["titulo"],
+                prj["conteudo"] or "—"
+            )
+
+        console.print(table)
+    except:
+        console.print(Panel("Tivemos um erro no processo... tente novamente!", style="bold red"))
+
+@project_app.command("view")
+def view_project(id: int):
+    console.print(Panel("Listar um projeto específica", style="bold blue"))
+
+    project = list_project_unique(id)
+
+    try:
+        table = Table(border_style="blue")
+
+        table.add_column("Título", style="white")
+        table.add_column("Descrição", style="dim")
+
+        table.add_row(
+            project["titulo"],
+            project["conteudo"] or "—"
+        )
+
+        console.print(table)
+    except:
+        console.print(Panel("Este projeto ainda não foi criado :(", style="bold red"))
+
+@project_app.command("view")
+def view_project(id: int, tasks: bool = typer.Option(False, "--tasks")):
+    if tasks:
+        result = list_tasks_project(id)
+
+        if not result:
+            console.print(Panel("Projeto não encontrado ou sem tarefas :(", style="bold red"))
+            return
+
+        console.print(Panel(f"Tasks do Projeto #{id}", style="bold blue"))
+
+        table = Table(border_style="blue")
+        table.add_column("ID", style="cyan", justify="center")
+        table.add_column("Título", style="white")
+        table.add_column("Descrição", style="dim")
+
+        for task in result:
+            table.add_row(
+                str(task["task_id"]),
+                task["titulo"],
+                task["conteudo"] or "—"
+            )
+
+        console.print(table)
+
+    else:
+        result = list_project_unique(id)
+
+        if not result:
+            console.print(Panel("Projeto não encontrado :(", style="bold red"))
+            return
+
+        console.print(Panel(f"Projeto #{id}", style="bold blue"))
+
+        table = Table(border_style="blue")
+        table.add_column("Nome", style="cyan")
+        table.add_column("Descrição", style="white")
+
+        table.add_row(result["titulo"], result["conteudo"] or "—")
+
+        console.print(table)
 
 if __name__ == '__main__':
     app()
